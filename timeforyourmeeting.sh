930ec387a07ddc5a96141b146a9c14d75b8a9e4e
somefake name <somefake@name.com>
2021-04-21 15:14:41 -0400

    Add cancel with ctrl+d, and make instructions more verbose.

--- timeforyourmeeting.sh
+++ timeforyourmeeting.sh
@@ -2,21 +2,37 @@
 #set -x
 
 # trap and quit
-trap ctrl_c INT
+trap ctrl_c SIGINT
 
 NEXT=""
 
 function ctrl_c()
 {
-  echo -e "\n\nHappy meeting day!"
+  echo -e "\n\nHappy meeting day!\n"
   exit 0
 }
 
-echo -e "\nLet's set some alerts.. ctrl+c to quit.\n"
+function eof_c()
+{
+  echo -e "\n\n** Deleting all meeting alerts! **\n\n"
+  for i in `atq | awk '{print $1}'`; do atrm $i; done
+  echo -e "... done, gbye!\n"
+  exit 0
+}
+
+echo -e "\nLet's set some alerts [24hr clock].."
+echo -e " -- ctrl+c to quit.\n -- ctrl+d to cancel all alerts.\n"
 
 while true; do
     echo -e "\nEnter your$NEXT meeting time:"    
+
     read meeting_time
+    
+    # check for ctrl-D, read exit code '1'
+    if [[ $? == 1 ]]; then 
+      eof_c
+    fi
+
     echo "spd-say -w 'time for your meeting'" | at $meeting_time > /dev/null 2>&1
     NEXT=" next"
 done
